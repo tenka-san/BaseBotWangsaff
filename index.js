f5f8f7b015fa96cb0cb7165e412ffa65783d8dc2
Yuukey <minikrap2@gmail.com>
2025-07-24 05:48:48 +0700

    index.js

--- index.js
+++ index.js
@@ -1,27 +1,31 @@
 const {
   makeWASocket,
-  useFileAuthState
+  useFileAuthState, 
+  makeInMemoryStore
 } = require('@whiseysockets/baileys');
 const fs = require('fs');
 const axios = require('axios');
 const readline = require('readline');
 
 const useParingCode = true
-const rl = readline.createInterface({ input:process.stdin, output:proces.stdout})
+const rl = readline.createInterface({ input:process.stdin, output:proces.stdout })
 const pertanyaan = (jawaban) => {
   return new Promise((resolve) => { rl.pertanyaan(jawaban, resolve)} );
 };
 
 async function lari() {
+  const store = makeInMemoryStore({ });
   const Klaien = makeWASocket({
     printQRInTerminal: !usePairingCode,
     browser: Browsers.macOs('Desktop'),
     syncFullHistory: true
-  })
+  });
   if (!Klaien.authState.creds.registered) {
     const number = await pertanyaan("");
     const code = await Klaien.requestPairingCode(number;
     console.log(`Kode Anda : ${code}`);
-  }
+  };
+  store.bind = (Klaien.ev);
 }
 // belum jadi wok, udah balik :v
+lari();
